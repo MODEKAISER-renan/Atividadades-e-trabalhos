d6481cecb114d7f37dc97fa45ce884704f32e83f
MODEKAISER-renan <20251ctb0100038@estudantes.ifpr.edu.br>
2026-02-19 17:25:11 +0000

    a

--- atividades do berssa/berssa atividade 01.py
+++ atividades do berssa/berssa atividade 01.py
@@ -1,3 +1,4 @@
+#Variaves previamente declaradas
 cont = 1
 valor = 0
 valor2 = 0
@@ -5,19 +6,25 @@ saida = 0
 ax = 0
 msg = ()
 opcoes = ()
-
+#funções de operação e verificação
 def soma(valor,valor2):
         print("o resultado da operação é = ",valor + valor2)
 def subtração(valor,valor2):
         print("o resultado da operação é = ",valor - valor2)
 def multiplicacao(valor,valor2):
         print("o resultado da operação é = ",valor * valor2)
+def divisao(valor,valor2):
+        try:
+               print("o resultado da operação é = ",valor / valor2)
+        except:
+               print("não é posivel dividir por zero")
+
 def msgs(msg):
         cont2 = 0
         for i in (msg):
                 print(f"*{msg[cont2]}*\n")
                 cont2 += 1
-def vx(opcoes): # sistema de verificação expecifico
+def vx(opcoes): # sistema de verificação expecifico numerico
         global ax
         cont = 1
         while cont == 1:
@@ -32,7 +39,7 @@ def vx(opcoes): # sistema de verificação expecifico
                                 return ax
                         else:
                                 print("Opção invalida")
-def verificaoN(msg):
+def verificaoN(msg):#sistema de verificação abrangente numerico
        global saida
        cont = 1
        while cont == 1:
@@ -44,25 +51,27 @@ def verificaoN(msg):
                        cont = 0
                        print("Opção valdia")
                        return saida
-
+#código realmente funcionando
 while cont == 1:
-    msg = [" o que quer fazer? ","somar[1]","subtrair[2]","multiplicação[3]","nenhuma das opções(terminar o código)[4]"]
-    msgs(msg)
-    opcoes = (1,2,3,4)
-    vx(opcoes)
-    if ax == 4:
-          cont = 0
-          break
-    msg = "digite o valor 1:"
-    verificaoN(msg)
-    valor = saida
-    msg = "digite o valor 2:"
-    verificaoN(msg)
-    valor2 = saida
-    if ax == 1:
-        soma(valor,valor2)
-    if ax == 2:
-        subtração(valor,valor2)
-    if ax == 3:
-       multiplicacao(valor,valor2)
+        msg = [" o que quer fazer? ","somar[1]","subtrair[2]","multiplicação[3]","Divisão[4]","nenhuma das opções(terminar o código)[5]"]
+        msgs(msg)
+        opcoes = (1,2,3,4,5)
+        vx(opcoes)
+        if ax == 5:
+                cont = 0
+                break
+        msg = "digite o valor 1:"
+        verificaoN(msg)
+        valor = saida
+        msg = "digite o valor 2:"
+        verificaoN(msg)
+        valor2 = saida
+        if ax == 1:
+                soma(valor,valor2)
+        if ax == 2:
+                subtração(valor,valor2)
+        if ax == 3:
+                multiplicacao(valor,valor2)
+        if ax == 4:
+                divisao(valor,valor2)
 print("Agradeço por usar esse código")
